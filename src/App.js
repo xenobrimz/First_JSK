59fa81d2a3855a8bda6dc0edae63e19ee0c73f37
XenoTheClapper <bobbyjackson13@live.com>
2021-07-08 11:13:39 -0500

    push

--- src/App.js
+++ src/App.js
@@ -3,21 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">   
+      <h1>Hello Dojo!</h1>
+      <h3>Things To Do:</h3>
+      <table>
+        <li>Learn react</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the Dogs</li>
+      </table>
     </div>
   );
 }
